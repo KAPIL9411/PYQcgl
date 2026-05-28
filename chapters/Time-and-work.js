205fb7430c17d0cde990ef1f5f5d12cfbe4080b7
KAPIL9411 <kurmikapil154@gmail.com>
2026-05-28 13:43:17 +0530

    Updated project6

---
+++ chapters/Time-and-work.js
@@ -0,0 +1,9 @@
+window.BASE_CHAPTERS = window.BASE_CHAPTERS || [];
+window.BASE_CHAPTERS.push({
+  id: "Time and Work",
+  title: "Time and Work",
+  desc: "SSC CGL PYQs focused on Time and Work.",
+  questions: [
+
+  ]
+});
